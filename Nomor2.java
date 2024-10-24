0ebc1d2568b5b627b5add5e108977b703bafc0f1
M Haykal Lazuardy <115679642+Valen754@users.noreply.github.com>
2024-10-24 08:40:39 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,63 @@
-public class Nomor2{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int trip = 0;
+        int lamatrip = 0;
+        double omzet = 0;
+        String driver;
+
+        for(int i = 0;  i <= 3; i++) {
+            System.out.println("Masukkan  nama driver:");
+            driver  = input.nextLine();
+            System.out.println("lama trip (km):");
+            lamatrip = input.nextInt();
+            switch (driver) {
+                case Beliau:
+                        if (lamatrip > 0){
+                            omzet = lamatrip * 13000;
+                        }
+
+                    break;
+                case Mereka:
+                        if (lamatrip > 0){
+                            omzet = lamatrip * 13000;
+                        }
+
+                    break;
+                case Kita:
+                        if (lamatrip > 0){
+                            omzet = lamatrip * 13000;
+                        }
+
+                    break;
+                default:
+                    System.out.println("Nama yang anda masukkan tidak valid");
+                    break;
+            }
+
+        }
+        omzet = Integer.MIN_VALUE;
+        trip += lamatrip;
+        System.out.println("Total trip yang dilakukan driver = " + trip);
+        System.out.println("Omzet tertinggi diperoleh oleh " + omzet);
     }
 }
+
+
+/*Pseudocode
+ * deklarasi:
+ * var trip,lamatrip;integer
+ * var omzet;double
+ * var driver;string
+ * var  i;integer
+
+ * 
+ * algoritma:
+ * for(i =  0; i <= 3; i++) {
+ *      switch
+ * }
+ * 
+ */
