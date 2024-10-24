73788affdd48e1ea23019fecf99dd30faad4e2e7
M Haykal Lazuardy <115679642+Valen754@users.noreply.github.com>
2024-10-24 07:34:03 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,36 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int jamlembur = 0;
+        int honorlembur = 0 ;
+        int totallembur = 0;
+
+        System.out.println("Masukkan  jam lembur : ");
+        jamlembur = scanner.nextInt();
+
+        if (jamlembur > 0  && jamlembur <= 16) {
+
+                honorlembur = jamlembur * 10000;
+        }
+        else if(jamlembur >  16){
+            honorlembur =  jamlembur * 15000;
+
+        }
+        else {
+
+        }
+
+        totallembur += honorlembur;
+
+
+        System.out.println("Total jam lembur:  " + jamlembur);
+        System.out.println("Total honor lembur:  " + totallembur);
+
+
+        scanner.close();
     }
 }
